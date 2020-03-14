c04dae553a1eee1e72b59a658a244ec6c0bdcc51
Tevin Stowers <tstowers@Tevins-MacBook-Pro-2.local>
2020-03-14 08:16:46 -0400

    first commit

---
+++ showMyLove.js
@@ -0,0 +1,16 @@
+function showLove() {
+    let myName = prompt("Whats is your name?");
+    const favColor = prompt("What is your favorite color?");
+    if (favColor == "red") {
+        window.alert("we are compatible!");
+    }
+    else if (favColor == "blue") {
+        window.alert("we are compatible!");
+    }
+    else if (favColor == "black") {
+        window.alert("we are compatible!");
+    }
+    else {
+        window.alert("we are not compatible!");
+    }
+}
